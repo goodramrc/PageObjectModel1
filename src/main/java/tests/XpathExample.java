182060a8110035fbf37e6153a4d2c2257ee68322
mariusrc <maryusrosca@gmail.com>
2024-06-07 18:07:43 +0300

    curs27_part2

--- src/main/java/tests/XpathExample.java
+++ src/main/java/tests/XpathExample.java
@@ -1,114 +1,127 @@
 package tests;
 
-import framework.utils.PropertiesFileProcessor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import framework.utils.PropertiesFileProcessor;
 import selenium.utils.BaseTest;
 
-public class XpathExample extends BaseTest {
+public class XpathExample extends BaseTest{
 
-    @Test(priority=1)
+    String USER = PropertiesFileProcessor.readPropertiesFile("user", "credentials.properties");
+    String PASS = PropertiesFileProcessor.readPropertiesFile("pass", "credentials.properties");
+
+
+    @Test(priority = 1)
     public void xpathExample1() throws InterruptedException {
-
-        String USER = PropertiesFileProcessor.readPropertiesFile("user", "credentials.properties");
-        String PASS = PropertiesFileProcessor.readPropertiesFile("pass", "credentials.properties");
         /*
-        Comparatie
-
-        CSS:
-        li[class='menu_user_login']
-
-        XPATH:
-        //li[@class='menu_user_login']
+         * CSS:
+         * 	li[class='menu_user_login']
+         *
+         * XPATH:
+         *   //li[@class='menu_user_login']
+         *
          */
 
         JavascriptExecutor jse = (JavascriptExecutor) driver;
 
-        WebElement loginBtn = driver.findElement(By.xpath("//li[@class='menu_user_login']"));
+        WebElement loginBtn = driver.findElement
+                (By.xpath("//li[@class='menu_user_login']"));
+
 
         jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')",
                 loginBtn);
 
         loginBtn.click();
 
-        //logical OR
 
-        WebElement userInput = driver.findElement(By.xpath("//ul//input[@id='log' or @name='log']"));
+        //logical OR
+        WebElement userInput = driver.findElement
+                (By.xpath("//ul//input[@id='log' or @name='log'] "));
 
         jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')",
                 userInput);
 
         userInput.sendKeys(USER);
 
-        WebElement passInput = driver.findElement(By.xpath("//ul//input[@type='password' and @name='pwd']"));
+        //logical AND
+
+        WebElement passInput = driver.findElement
+                (By.xpath("//ul//input[@type='password' and @name='pwd'] "));
+
         jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')",
                 passInput);
 
         passInput.sendKeys(PASS);
 
         // direct child
-
-        WebElement submitBtn = driver.findElement(By.xpath("//ul//div[@class='popup_form_field submit_field']/input"));
+        WebElement submitBtn = driver.findElement
+                (By.xpath("//ul//div[@class='popup_form_field submit_field']/input"));
 
         submitBtn.click();
 
         Thread.sleep(5000);
-
     }
 
-    @Test(priority = 2)
+    @Test(priority=2)
     public void xpathExample2() {
         JavascriptExecutor jse = (JavascriptExecutor) driver;
 
-        driver.findElement(By.xpath("//span[@class='user_name']"));
-
-
-        driver.findElement(By.xpath("/a[text()='Settings']")).click();
+        driver.findElement(By.xpath("//span[@class='user_name']")).click();
 
         //<a href="https://keybooks.ro/wp-admin/profile.php" class="icon icon-cog">Settings</a>
         /*
-         //a[text()='Settings']
+         *  //a[text()='Settings']
          */
-        // text()--> referinta catra textul dintre tag-urile HTML
+        // text()--> referinta catre textul dintre tag-urile HTML
+        driver.findElement(By.xpath("//a[text()='Settings']")).click();
 
+
+        /*
+         * <a href="https://keybooks.ro/account/orders/">recent orders</a>
+         *
+         *  //a[text()='recent orders']
+         *
+         *  //a[contains(text(), 'recen')]
+         *
+         *  //a[contains(@href, 'orders')]
+         *
+         */
         //contains()
-        WebElement recentOrderLink = driver.findElement(By.xpath("//a[contains(@href, 'orders')]"));
+        WebElement recentOrderLink = driver.findElement
+                (By.xpath("//a[contains(text(), 'recen')]"));
 
-        jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')",
+        jse.executeScript("arguments[0].setAttribute('style', "
+                        + "'background:yellow ;border:10px solid green')",
                 recentOrderLink);
 
         recentOrderLink.click();
 
-        WebElement ordersTableLink = driver.findElement(By.xpath("//th[contains(@class, 'orders-table__header')]/span[text()='Order']"));
+        WebElement ordersTableLink = driver.findElement
+                (By.xpath("//th[contains(@class, 'orders-table__header')]/span[text()='Order']"));
 
-        jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')",
+        jse.executeScript("arguments[0].setAttribute('style', "
+                        + "'background:yellow ;border:10px solid green')",
                 ordersTableLink);
 
         //not()
-        WebElement dateTableHeader = driver.findElement(By.xpath("//table//span[not(contains(text(), 'Order')) and not(contains(text(), 'Status')) and not(contains(@class, 'woocommerce')) and not(contains(text(), 'Total')) and not(contains(text(), 'Actions')) ]"));
+        WebElement dateTableHeader = driver.findElement
+                (By.xpath("//table//span[not(contains(text(), 'Order')) and not(contains(text(), 'Status')) and not(contains(@class, 'woocommerce')) and not(contains(text(), 'Total')) and not(contains(text(), 'Actions')) ]"));
 
-        jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')",
+        jse.executeScript("arguments[0].setAttribute('style', "
+                        + "'background:yellow ;border:10px solid green')",
                 dateTableHeader);
 
-        //index()
-        WebElement statusTable = driver.findElement(By.xpath("(//span[@class='nobr'])[3]"));
+        //index
+        WebElement statusTableHeader = driver.findElement
+                (By.xpath("(//span[@class='nobr'])[3]"));
 
-        jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')",
-                statusTable);
+        jse.executeScript("arguments[0].setAttribute('style', "
+                        + "'background:yellow ;border:10px solid green')",
+                statusTableHeader);
 
     }
-
-   //<a href="https://keybooks.ro/account/orders/">recent orders</a>
-   /*
-   //a[text()='recent orders']
-
-   //a[contains(text(), 'recent')]
-
-   //a[contains(@href, 'orders')]
-    */
-
-
 
 }
